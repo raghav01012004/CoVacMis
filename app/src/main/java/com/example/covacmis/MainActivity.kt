fe6fe0b6ba454241c2953c7d7eb914901a209def
MCUxKILLER <svgamer7781@gmail.com>
2023-10-30 10:43:36 +0530

    Backend connection to sign up and vaccine detail screen

--- app/src/main/java/com/example/covacmis/MainActivity.kt
+++ app/src/main/java/com/example/covacmis/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.covacmis
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +27,52 @@ class MainActivity : AppCompatActivity() {
 
 
         button.setOnClickListener {
-            val uname = username.text
-            val pass = password.text
+            val uname = username.text?.toString()
+            val pass = password.text?.toString()
 
-            if(uname.toString() == "" || pass.toString() == "")
-            {
-                Toast.makeText(applicationContext, "Please fill the above fields", Toast.LENGTH_SHORT).show()
+            if (uname.isNullOrEmpty() || pass.isNullOrEmpty()) {
+                Toast.makeText(
+                    applicationContext,
+                    "Please fill the above fields",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                getData(uname, pass)
             }
-            else{
-                startActivity(Intent(applicationContext,VaccinationChart::class.java))
-            }
-
 
         }
         textView.setOnClickListener {
             Toast.makeText(applicationContext, "Wait a sec...", Toast.LENGTH_SHORT).show()
 
-            intent = Intent(applicationContext,MainActivity2::class.java)
+            intent = Intent(applicationContext, MainActivity2::class.java)
             startActivity(intent)
         }
 
+
     }
+
+    private fun getData(username: String, password: String) {
+        // Assuming you have `username` and `password` as Strings
+        val url = "http://10.0.2.2:8000/login/$username/$password"
+        val request = JsonObjectRequest(Request.Method.GET, url, null,
+            { response ->
+                val user = Gson().fromJson(response.toString(), User::class.java)
+//                for(i in response.keys()){
+//                    println(response[i])
+//                }
+                if (user?.fullname != null && user.fullname.isNotEmpty()) {
+                    val intent = Intent(applicationContext, VaccinationChart::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Incorrect username or password", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            },
+            { error ->
+                Log.d("MainActivity", error.toString())
+            })
+        val queue = Volley.newRequestQueue(this)
+        queue.add(request)
+    }
+
 }
--- app/src/main/java/com/example/covacmis/VaccinationChart.kt
+++ app/src/main/java/com/example/covacmis/VaccinationChart.kt
@@ -1,52 +1,75 @@
 package com.example.covacmis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import org.json.JSONArray
 
 class VaccinationChart : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var dataList:ArrayList<DataClass>
-    private lateinit var vaccineName:Array<String>
-    private lateinit var ageGroup:Array<String>
+    private lateinit var vaccineName:ArrayList<String>
+    private lateinit var ageGroup:ArrayList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vaccination_chart)
 
-        vaccineName = arrayOf(
-            "BCG",
-            "VARICELLA",
-            "ORAL POLIO",
-            "HEPATITIS B",
-            "TDAP",
-        )
+        vaccineName = arrayListOf()
 
-        ageGroup = arrayOf(
-            "0 - 1",
-            "1 - 2",
-            "9 - 45",
-            "6 - 12",
-            "1 - 3",
-        )
+        ageGroup = arrayListOf()
 
         recyclerView = findViewById(R.id.recyclerview)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
 
         dataList = arrayListOf<DataClass>()
-        getData()
-
+        getVaccines()
     }
 
 
+    private fun getVaccines(){
+        val url = "http://10.0.2.2:8000/vaccines"
 
-    private fun getData(){
-        for(i in 0..4){
-            val dataClass = DataClass(vaccineName[i],ageGroup[i])
-            dataList.add(dataClass)
-        }
-        recyclerView.adapter = AdapterClass(dataList)
+        val request = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+
+                    for (key in response.keys()) {
+
+                        val resultString = when (val value = response.get(key)) {
+                            is JSONArray -> {
+                                val element1 = value.opt(0) as? Number
+                                val element2 = value.opt(1) as? Number
+
+                                when {
+                                    element1 == null && element2 == null -> "No age"
+                                    element1 == element2 -> element1.toString()
+                                    else -> ">${element1 ?: element2}"
+                                }
+                            }
+                            else -> "any"
+                        }
+
+                        val dataClass = DataClass(key,resultString)
+                        dataList.add(dataClass)
+                    }
+                recyclerView.adapter = AdapterClass(dataList)
+
+            },
+            { error ->
+                Log.d("VaccinationChart", error.toString())
+            })
+
+        val queue = Volley.newRequestQueue(this)
+        queue.add(request)
     }
 }
